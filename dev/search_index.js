7e2b226482f07dd62bfac62b2bb091b82ec9fee8
Documenter.jl <documenter@juliadocs.github.io>
2022-09-20 02:36:44 +0000

    build based on aee138e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RDM","category":"page"},{"location":"#RDM","page":"Home","title":"RDM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RDM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RDM]","category":"page"}]
+}
