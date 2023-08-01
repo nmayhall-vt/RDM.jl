5312b235469707e42a032f1bdc3fcb4349135672
Arnab <ayush@Arnab.local>
2023-08-01 15:22:22 -0400

    test added for hessian

---
+++ test/test_hessian.jl
@@ -0,0 +1,32 @@
+using QCBase
+using RDM  
+using Test
+using NPZ
+using Printf
+using InCoreIntegrals
+using JLD2
+using LinearAlgebra
+using Random
+
+
+@testset "RDM" begin
+
+    @load "data_fd_hessian.jld2"
+    orbital_hessian=RDM.build_orbital_hessian(ints,gd1,gd2)
+    num_hess_function=num_hess1.*projection_matrix# finite difference hessian using objective function
+    num_hess_gradient=num_hess2.*projection_matrix# finite difference hessian using gradient
+    orbital_hessian=orbital_hessian.*projection_matrix
+    
+    println(" \n Analytical Hessian: ")
+    display(orbital_hessian)
+    eigenvalue_hessian=eigvals(orbital_hessian)
+    display(eigenvalue_hessian)
+    @test all(x-> x>=0.0,eigenvalue_hessian)
+    println("\n Finite difference Hessian")
+    display(num_hess_function)
+    display(num_hess_gradient)
+    
+    @test isapprox(norm(orbital_hessian-num_hess_function), 0.0, atol=1e-4)
+    @test isapprox(norm(orbital_hessian-num_hess_gradient), 0.0, atol=1e-6)
+    @test isapprox(norm(num_hess_function-num_hess_gradient), 0.0, atol=1e-4)
+end
